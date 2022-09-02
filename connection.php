f9fbe68a963f4c583cef54d5b53e389248181d81
Ziyauddin Shaik <112611879+Ziyauddin189@users.noreply.github.com>
2022-09-02 10:17:55 +0530

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,12 @@
+<?php
+
+$dbhost = "sql6.freesqldatabase.com";
+$dbuser = "sql6516574";
+$dbpass = "qIvV3dYg1s";
+$dbname = "sql6516574";
+
+if(!$con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname))
+{
+	die("failed to connect!");
+}
+?>
